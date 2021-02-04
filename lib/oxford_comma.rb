4279876f384a5687e037bbcd43c591a8a1a08117
Bryanjazo <bryanjazo88@gmail.com>
2021-02-04 15:04:44 -0800

    commit tree 8fb76494004be72c8cceb79aa0c5376ee7b25f69

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+require "pry"
 def oxford_comma(array)
-
+    #binding.pry
+    if array.length == 1 
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        array[-1] = "and " + array[-1]
+        array.join(", ")
+    end
 end
